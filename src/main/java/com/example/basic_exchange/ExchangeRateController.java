f0e6dae7893d9a4df40220207c0e6d07d6205e28
Simon <sleroux@carboniaweb.com>
2023-11-22 18:46:38 -0500

    Update api url to match requirements (no api prefix)

--- src/main/java/com/example/basic_exchange/ExchangeRateController.java
+++ src/main/java/com/example/basic_exchange/ExchangeRateController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/exchange-rate")
+@RequestMapping("/exchange-rate")
 public class ExchangeRateController {
 
     private final ExchangeRateService exchangeRateService;
